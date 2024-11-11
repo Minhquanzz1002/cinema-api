7a88104f2324924ea465f35de9a4b49ee7ece17f
Nguyễn Minh Quân <quannguyenminh1001@gmail.com>
2024-11-11 11:23:59 +0700

    fix: get all employee

--- src/main/java/vn/edu/iuh/constant/SwaggerConstant.java
+++ src/main/java/vn/edu/iuh/constant/SwaggerConstant.java
@@ -10,4 +10,11 @@ public class SwaggerConstant {
     // FOR ADMIN PAYMENT
     public static final String POST_ADMIN_PAYMENT_ZALOPAY_SUM = "Tạo đơn hàng ZaloPay";
     public static final String GET_ADMIN_PAYMENT_ZALOPAY_SUM = "Lấy thông tin đơn hàng ZaloPay";
+
+    // FOR ADMIN EMPLOYEE
+    public static final String GET_ALL_ADMIN_EMPLOYEE_SUM = "Danh sách nhân viên";
+    public static final String GET_ADMIN_EMPLOYEE_SUM = "Lấy thông tin nhân viên";
+    public static final String POST_ADMIN_EMPLOYEE_SUM = "Thêm mới nhân viên";
+    public static final String PUT_ADMIN_EMPLOYEE_SUM = "Cập nhật thông tin nhân viên";
+    public static final String DELETE_ADMIN_EMPLOYEE_SUM = "Xóa nhân viên";
 }
--- src/main/java/vn/edu/iuh/services/EmployeeService.java
+++ src/main/java/vn/edu/iuh/services/EmployeeService.java
@@ -5,12 +5,14 @@ import org.springframework.data.domain.Pageable;
 import vn.edu.iuh.dto.admin.v1.req.CreateEmployeeDTO;
 import vn.edu.iuh.dto.admin.v1.req.EmployeeResponseDTO;
 import vn.edu.iuh.dto.admin.v1.req.UpdateEmployeeDTO;
+import vn.edu.iuh.models.enums.BaseStatus;
+import vn.edu.iuh.models.enums.UserStatus;
 
 
 import java.util.UUID;
 
 public interface EmployeeService {
-    Page<EmployeeResponseDTO> getEmployees(String search, Pageable pageable);
+    Page<EmployeeResponseDTO> getEmployees(String search, UserStatus status, Pageable pageable);
 
     EmployeeResponseDTO getEmployee(UUID id);
 
